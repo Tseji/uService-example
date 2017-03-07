8a2135158abe27ae9f69eb9b564349b3506ee1d3
Tseji <TseJi@esquel.com>
2017-03-07 15:09:54 +0800

    Initial check-in

---
+++ swagger/src/main/java/io/swagger/configuration/security/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package io.swagger.configuration.security;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.swagger.configuration.security.jwt.JWTAuthenticationFilter;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+     
+    	// disable caching
+        http.headers().cacheControl();
+        
+        http.csrf().disable() // disable csrf for our requests.
+    	 .authorizeRequests()
+    	  // .antMatchers("/v2/**").permitAll()
+    	 //  .antMatchers(HttpMethod.POST, "/login").permitAll()
+    	   .anyRequest().authenticated()
+    	   .and()
+    	  // And filter other requests to check the presence of JWT in header
+           .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        
+    	
+    }
+
+  
+}
---
+++ swagger/src/main/java/io/swagger/configuration/security/jwt/TokenAuthenticationService.java
@@ -0,0 +1,60 @@
+package io.swagger.configuration.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+
+public class TokenAuthenticationService {
+
+    private String secret = "VGhpc0lzQVNlY3JldA=="; // base64 encoded "ThisIsASecret"
+    private String headerString = "Authorization";
+ 
+
+    public Authentication getAuthentication(HttpServletRequest request)  
+    {
+    	   	
+    	
+        String token = request.getHeader(headerString);
+        if(token != null)
+        {
+        	
+        	System.out.println("Token Received:"+token);
+        	
+            // parse the token.
+            String username = Jwts.parser()
+                        .setSigningKey(secret)
+                        .parseClaimsJws(token)
+                        .getBody()
+                        .getSubject();
+            
+            System.out.println(username);
+            
+            Jws<Claims> claims = Jwts.parser()
+            		  .setSigningKey(secret)
+            		  .parseClaimsJws(token);
+            
+            		String scope = (String) claims.getBody().get("scope");
+            		System.out.println(scope);
+            		//assertEquals(scope, "self groups/admins");
+            
+             Date exp = Jwts.parser()
+          		  .setSigningKey(secret)
+          		  .parseClaimsJws(token).getBody().getExpiration();
+             
+                  System.out.println("Expiry Date:" + exp);
+            
+            if(username != null) // we managed to retrieve a user
+            {
+                return new AuthenticatedUser(username);
+            }
+        }
+        return null;
+    }
+}
